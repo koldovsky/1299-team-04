7ef7e152b39dea2f69f246f75584bfd037f73eb3
Yuriy Romanchuk <romanchuk.yura@gmail.com>
2024-11-24 13:57:37 +0200

    add js in my partial

--- js/index.section-our-fave-finds.js
+++ js/index.section-our-fave-finds.js
@@ -1,118 +1,110 @@
 const products = [
   {
     id: '1',
-    className: 'Chairs',
-    ourName: 'Fusion armchair, LEXX',
-    ourImg: 'img/section-our-fave-finds/armchairs/fusion-yellow-chair.png',
-    ourAlt: 'Yellow chair on metal legs',
-    ourPrice: '320.00',
+    name: 'Fusion armchair, LEXX',
+    image: 'img/section-our-fave-finds/armchairs/fusion-yellow-chair.png',
+    description: 'Yellow chair on metal legs',
+    price: '320.00',
   },
   {
     id: '2',
-    className: 'Chairs',
-    ourName: 'Aurora armchair, MDS',
-    ourImg: 'img/section-our-fave-finds/armchairs/jeanine-gray-chair.png',
-    ourAlt: 'Beige tufted armchair with wooden legs and curved armrests',
-    ourPrice: '210.00',
+    name: 'Aurora armchair, MDS',
+    image: 'img/section-our-fave-finds/armchairs/jeanine-gray-chair.png',
+    description: 'Beige tufted armchair with wooden legs and curved armrests',
+    price: '210.00',
   },
   {
     id: '3',
-    className: 'Chairs',
-    ourName: 'Cloud armchair, DIX',
-    ourImg: 'img/section-our-fave-finds/armchairs/cloud-tiffany-chair.png',
-    ourAlt: 'Tiffany soft chair with wooden legs and armrests',
-    ourPrice: '150.00',
+    name: 'Cloud armchair, DIX',
+    image: 'img/section-our-fave-finds/armchairs/cloud-tiffany-chair.png',
+    description: 'Tiffany soft chair with wooden legs and armrests',
+    price: '150.00',
   },
   {
     id: '4',
-    className: 'Chairs',
-    ourName: 'Jeanine armchair, KUKO',
-    ourImg: 'img/section-our-fave-finds/armchairs/jeanine-gray-chair.png',
-    ourAlt: 'Light gray soft chair with wooden legs and armrests',
-    ourPrice: '210.00',
+    name: 'Jeanine armchair, KUKO',
+    image: 'img/section-our-fave-finds/armchairs/jeanine-gray-chair.png',
+    description: 'Light gray soft chair with wooden legs and armrests',
+    price: '210.00',
   },
   {
     id: '5',
-    className: 'lamps',
-    ourName: '3-piece Minimal, ARM',
-    ourImg: 'img/section-our-fave-finds/lamps/white-lamps.png',
-    ourAlt: 'Three exquisite white lamps',
-    ourPrice: '110.00',
+    name: '3-piece Minimal, ARM',
+    image: 'img/section-our-fave-finds/lamps/white-lamps.png',
+    description: 'Three exquisite white lamps',
+    price: '110.00',
   },
   {
     id: '6',
-    className: 'lamps',
-    ourName: 'Red Drops, KUKO',
-    ourImg: 'img/section-our-fave-finds/lamps/red-lamps.png',
-    ourAlt: 'Two red lamps in the form of drops',
-    ourPrice: '210.00',
+    name: 'Red Drops, KUKO',
+    image: 'img/section-our-fave-finds/lamps/red-lamps.png',
+    description: 'Two red lamps in the form of drops',
+    price: '210.00',
   },
   {
     id: '7',
-    className: 'lamps',
-    ourName: 'Delar, Uni Studio',
-    ourImg: 'img/section-our-fave-finds/lamps/decor-lamps.png',
-    ourAlt: 'Two lamps decorated in gold',
-    ourPrice: '350.00',
+    name: 'Delar, Uni Studio',
+    image: 'img/section-our-fave-finds/lamps/decor-lamps.png',
+    description: 'Two lamps decorated in gold',
+    price: '350.00',
   },
   {
     id: '8',
-    className: 'lamps',
-    ourName: 'Bubbles, Udion',
-    ourImg: 'img/section-our-fave-finds/lamps/gold-lamps.png',
-    ourAlt: 'Three decorative light bulbs in the form of golden beads',
-    ourPrice: '410.00',
+    name: 'Bubbles, Udion',
+    image: 'img/section-our-fave-finds/lamps/gold-lamps.png',
+    description: 'Three decorative light bulbs in the form of golden beads',
+    price: '410.00',
   },
   {
     id: '9',
-    className: 'Tables',
-    ourName: 'Upi table, Landstrom',
-    ourImg: 'img/section-our-fave-finds/tables/white-table.png',
-    ourAlt: 'A white table with a green vase on it',
-    ourPrice: '90.00',
+    name: 'Upi table, Landstrom',
+    image: 'img/section-our-fave-finds/tables/white-table.png',
+    description: 'A white table with a green vase on it',
+    price: '90.00',
   },
   {
     id: '10',
-    className: 'Tables',
-    ourName: 'Monica table, Orro',
-    ourImg: 'img/section-our-fave-finds/tables/gold-table.png',
-    ourAlt: 'A gold metal table on gold metal legs on which a jug is placed',
-    ourPrice: '110.00',
+    name: 'Monica table, Orro',
+    image: 'img/section-our-fave-finds/tables/gold-table.png',
+    description: 'A gold metal table on gold metal legs on which a jug is placed',
+    price: '110.00',
   },
   {
     id: '11',
-    className: 'Tables',
-    ourName: 'Snitch cabinet, ARM',
-    ourImg: 'img/section-our-fave-finds/tables/wood-table.png',
-    ourAlt: 'A wooden table with a vase on it',
-    ourPrice: '100.00',
+    name: 'Snitch cabinet, ARM',
+    image: 'img/section-our-fave-finds/tables/wood-table.png',
+    description: 'A wooden table with a vase on it',
+    price: '100.00',
   },
   {
     id: '12',
-    className: 'Tables',
-    ourName: 'Hugo table, KUKO studio',
-    ourImg: 'img/section-our-fave-finds/tables/gray-table.png',
-    ourAlt: 'A dark gray metal table on a round base made of many metal rods',
-    ourPrice: '99.00',
+    name: 'Hugo table, KUKO studio',
+    image: 'img/section-our-fave-finds/tables/gray-table.png',
+    description: 'A dark gray metal table on a round base made of many metal rods',
+    price: '99.00',
   },
 ];
 
-function displayProducts(className) {
-  const outputDiv = document.getElementById('output');
-
-  const filteredProducts = products.filter((product) => product.className === className);
-
-  outputDiv.innerHTML = filteredProducts.length
-    ? filteredProducts.map((product) =>
-        `<div>
-          <img src="${product.ourImg}" alt="${product.ourAlt}">
-          <h3>${product.ourName}</h3>
-          <p>Ціна: $${product.ourPrice}</p>
-        </div>`
-      ).join("")
-    : `<p>Немає товарів у класі ${className}</p>`;
+function displayProducts(products) {
+  let productsHtml = '';
+  for (const product of products) {
+    productsHtml += `
+      <div class="our-finds__product-card our-finds__product--armchairs">
+        <div class="our-finds__product-image">
+          <a href="#"><img src="${product.image}" alt="${product.description}" /></a>
+        </div>
+        <div class="our-finds__name-div">
+          <h3><a href="#" class="our-finds__name">${product.name}</a></h3>
+        </div>
+        <div class="our-finds__price-div">
+          <p class="our-finds__price">${product.price} <span class="our-finds__currency">USD</span></p>
+        </div>
+        <div class="our-finds__order-div">
+          <span class="order-button">Order Now</span>
+        </div>
+      </div>`;
+  }
+  document.querySelector('.our-finds__products').innerHTML = productsHtml;
 }
 
-document.getElementById("classChairs").addEventListener("click", () => displayProducts("Chairs"));
-document.getElementById("classLamps").addEventListener("click", () => displayProducts("Lamps"));
-document.getElementById("classTables").addEventListener("click", () => displayProducts("Tables"));
+displayProducts(products);
