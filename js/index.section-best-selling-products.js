eafd3d5d48e62cf6072cb5c4e0b2a23065312244
Kaznokh <113619266+MegaKonzo@users.noreply.github.com>
2024-12-02 14:46:57 +0000

    the state of the code before generating a new product details page

--- js/index.section-best-selling-products.js
+++ js/index.section-best-selling-products.js
@@ -48,25 +48,124 @@ renderProductsCard(products_card);
 const products_details = [
   {
     id: '1',
-    image: 'img/section-best-selling-products/white_armchair.png',
-    alt: 'Minimalist interior with a white armchair featuring a soft cushion, positioned next to a tripod floor lamp with a white lampshade and a decorative plant in a white pot on the floor. The walls and floor are also in white.',
     title: 'Lord armchair, Miedel Home (LAMH)',
     code: 'product code 14',
     price: '120,00USD',
-    description: 'Do you want to feel comfortable and hide from the bad weather? This armchair isperfect for relaxing with a book or a cup of tea. It also provides extra backsupport.'
+    description_1: 'Do you want to feel comfortable and hide from the bad weather? This armchair isperfect for relaxing with a book or a cup of tea. It also provides extra backsupport.',
+    description_2: 'Dimensions and materials:28" x 38"Faux suede, wood',
+    image: 'img/section-best-selling-products/white_armchair.png',
+    alt: 'Minimalist interior with a white armchair featuring a soft cushion, positioned next to a tripod floor lamp with a white lampshade and a decorative plant in a white pot on the floor. The walls and floor are also in white.',
   },
   {
     id: '2',
+    title: 'Ultimate Green chair, XODO (UGCX)',
+    code: 'Product code 15',
+    price: '90,00USD',
+    description_1: 'Simple yet functional, this chair is perfect for your minimalistic dining room. It will become a bright spot in your interior design and will awaken your appetite.',
+    description_2: 'Dimensions and materials:21" x 32"Plastic',
     image: 'img/section-best-selling-products/green_armchair.png',
     alt: 'Minimalist interior with a green chair placed next to a small black side table holding a glass vase with green leaves. Three hanging light bulbs are suspended above, and the background features a white wall and floor.',
-    title: 'Ultimate Greenchair, XODO',
-    price: '90,00USD',
   },
   {
     id: '3',
+    title: 'Valetta armchair, ZIX studio (VAZS)',
+    code: 'Product code 17',
+    price: '310,00USD',
+    description_1: 'An armchair in which you will feel what real comfort is. The original design will suit both the bedroom and the living room, or even a home office. Available only in pink.',
+    description_2: 'Dimensions and materials:28" x 35"Faux suede, steel',
     image: 'img/section-best-selling-products/pink_armchair.png',
     alt: 'Minimalist interior with a pink armchair featuring wooden legs, positioned next to a small round gold side table holding a small orange candle. A cluster of round pendant lights hangs above, with a white wall and floor in the background.',
-    title: 'Valetta armchair, ZIX studio',
-    price: '310,00USD',
   },
 ];
+
+function renderProductsDetails(products_details) {
+  let productsHtml = '';
+  for (const product of products_details) {
+    productsHtml += `    
+          <div class="product__gallery">
+            <button class="arrow left">
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round">
+              <polyline points="12 19 5 12 12 5"></polyline>
+            </svg>
+            </button>
+            <div class="container__img">  
+            </div>
+            <button class="arrow right">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </button>
+            <div class="thumbnail__container">
+              <img
+                src="img/section-best-selling-products/white_armchair.png"
+                alt="Thumbnail 1"
+                class="thumbnail active"
+              />
+              <img
+                src="img/section-best-selling-products/green_armchair.png"
+                alt="Thumbnail 2"
+                class="thumbnail"
+              />
+              <img
+                src="img/section-best-selling-products/white_armchair.png"
+                alt="Thumbnail 3"
+                class="thumbnail"
+              />
+              <img
+                src="img/section-best-selling-products/pink_armchair.png"
+                alt="Thumbnail 4"
+                class="thumbnail"
+              />
+            </div>
+          </div>
+
+          <div class="product__details">
+            <p class="in__stock">In stock</p>
+            <h3 class="title">Lord armchair, Miedel Home (LAMH)</h3>
+            <p class="product__code">Product code 14</p>
+            <p class="price">120,00 USD</p>
+            <hr class="horizontal__line" />
+            <div class="quantity__add">
+              <input type="number" value="1" min="1" max="100" class="input__number" />
+              <button class="add__to__cart">ADD TO CART</button>
+            </div>
+            <div>
+              <h2 class="description">DESCRIPTION</h2>
+              <hr class="horizontal__line__description" />
+              <hr class="horizontal__line__description__after" />
+              <p class="description__info">
+                Do you want to feel comfortable and hide from the bad weather?<br>
+                This armchair is perfect for relaxing with a book or a cup of tea.<br>
+                It also provides extra back support.<br>
+              </p>
+              <p></p>
+              <p class="description__info">
+                Dimensions and materials:<br />
+                28" x 38"<br />
+                Faux suede, wood
+              </p>
+            </div>
+          </div>`;
+  }
+  document.querySelector('.products__list').innerHTML = productsHtml;
+}
+
+renderProductsCard(products_card);
