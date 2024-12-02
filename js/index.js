ab9884536687f06490083abc2b1286f68bb2289f
blyndaruk <dmitriy.blyndaruk@gmail.com>
2024-12-02 22:47:04 +0200

    feat: testimonials section

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function init() {
   import('./index.section-header.js');
   import('./index.section-our-fave-finds.js');
   import('./index.section-partners.js');
+  import('./index.section-testimonials.js');
   import('./index.section-best-selling-products.js');
   import('./index.section-best-selling-products-details.js');
   import('./index.section-contact-form-partial.js');
