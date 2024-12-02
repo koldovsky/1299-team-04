82f877fd25bd5da9147cfc8b28b9cd2c31143138
Kaznokh <113619266+MegaKonzo@users.noreply.github.com>
2024-12-02 14:21:26 +0000

    add @media

--- js/index.section-best-selling-products.js
+++ js/index.section-best-selling-products.js
@@ -32,7 +32,7 @@ function renderProductsCard(products_card) {
                     <img src="${product.image}" alt="${product.alt}"/>
                 </a>
             </div>
-            <a style="text-decoration: none " href="#">
+            <a style="text-decoration: none " href="index.section-best-selling-products-details.partial.html">
             <h3 class="product-card__title">${product.title}</h3></a>
             <p class="product-card__price">${product.price}</p>
             <button class="product-card__button" type="button">ORDER NOW</button>
@@ -42,3 +42,31 @@ function renderProductsCard(products_card) {
 }
 
 renderProductsCard(products_card);
+
+///RENDER PAGE FO DETAILs
+
+const products_details = [
+  {
+    id: '1',
+    image: 'img/section-best-selling-products/white_armchair.png',
+    alt: 'Minimalist interior with a white armchair featuring a soft cushion, positioned next to a tripod floor lamp with a white lampshade and a decorative plant in a white pot on the floor. The walls and floor are also in white.',
+    title: 'Lord armchair, Miedel Home (LAMH)',
+    code: 'product code 14',
+    price: '120,00USD',
+    description: 'Do you want to feel comfortable and hide from the bad weather? This armchair isperfect for relaxing with a book or a cup of tea. It also provides extra backsupport.'
+  },
+  {
+    id: '2',
+    image: 'img/section-best-selling-products/green_armchair.png',
+    alt: 'Minimalist interior with a green chair placed next to a small black side table holding a glass vase with green leaves. Three hanging light bulbs are suspended above, and the background features a white wall and floor.',
+    title: 'Ultimate Greenchair, XODO',
+    price: '90,00USD',
+  },
+  {
+    id: '3',
+    image: 'img/section-best-selling-products/pink_armchair.png',
+    alt: 'Minimalist interior with a pink armchair featuring wooden legs, positioned next to a small round gold side table holding a small orange candle. A cluster of round pendant lights hangs above, with a white wall and floor in the background.',
+    title: 'Valetta armchair, ZIX studio',
+    price: '310,00USD',
+  },
+];
