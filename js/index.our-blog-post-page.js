4de8ed2936ab84da71d2ac207b5ec67fbe3b0727
blyndaruk <dmitriy.blyndaruk@gmail.com>
2024-12-03 15:01:37 +0200

    fix: our blog links

--- js/index.our-blog-post-page.js
+++ js/index.our-blog-post-page.js
@@ -1,16 +1,20 @@
-const response = await fetch('api/posts-content.json'); 
+const response = await fetch('api/posts-content.json');
 const posts = await response.json();
+
 function render_posts_on_main(posts) {
-    let postsHtml = "";
-    for (const post of posts){
-        postsHtml += `
+  const repoName = '1299-team-04';
+  const isGitHubPages = window.location.pathname.includes(repoName);
+  const basePath = isGitHubPages ? `${window.location.origin}/${repoName}` : window.location.origin;
+  let postsHtml = '';
+  for (const post of posts) {
+    postsHtml += `
         <div class="main-posts__main-post">
-            <a href="${post.link}" class="blog-part"><img src="${post.image}" alt="${post.title}"></a>
+            <a href="${basePath}/${post.link}" class="blog-part"><img src="${post.image}" alt="${post.title}"></a>
             <p>${post.date}</p>
-            <a href="${post.link}" class="blog-part">
+            <a href="${basePath}/${post.link}" class="blog-part">
                 <h3>${post.title}</h3>
             </a>
-            <a href="${post.link}" class="blog-part">
+            <a href="${basePath}/${post.link}" class="blog-part">
                 <p>${post.mainText}</p>
             </a>
             <div class="main-posts__post-creator">
@@ -18,9 +22,10 @@ function render_posts_on_main(posts) {
                 <p>${post.author}</p>
             </div>
         </div>`;
-    }
-    document.querySelector('.our-blog__main-posts').innerHTML = postsHtml;
+  }
+  document.querySelector('.our-blog__main-posts').innerHTML = postsHtml;
 }
+
 render_posts_on_main(posts);
 
 
